df82168bc2fae956779eff1474882792076d774a
Alcino José Lincoln de Souza <lincoln_tec@hotmail.com>
2023-10-16 08:12:26 -0300

    Finalização aula 3

---
+++ src/main/java/com/br/lincolnsouza/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package com.br.lincolnsouza.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
